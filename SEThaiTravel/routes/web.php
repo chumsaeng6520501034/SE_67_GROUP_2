0302f2109e365f839e641f856303807db07f1b27
attsawin <attsawin.jan@gmail.com>
2025-03-04 22:31:28 +0700

    Update web.php

--- SEThaiTravel/routes/web.php
+++ SEThaiTravel/routes/web.php
@@ -37,3 +37,5 @@ Route::post('/checkLogIn',[AccountController::class,'checkLogin']);
 Route::get('/calendar',[UserListController::class,'viewCalendar']);
 Route::get('/myBooking',[UserListController::class,'viewMyBooking']);
 Route::post('/searchBooking',[UserListController::class,'searchBooking']);
+
+
