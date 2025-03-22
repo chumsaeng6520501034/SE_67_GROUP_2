eda68989982494e977f2a8bd9a22f7e04f8b77d5
attsawin <attsawin.jan@gmail.com>
2025-03-22 20:46:12 +0700

    format document +add tour

--- SEThaiTravel/app/Http/Controllers/CorpListController.php
+++ SEThaiTravel/app/Http/Controllers/CorpListController.php
@@ -1,17 +1,63 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Http\Request;
 use App\Models\CorpList;
+use Carbon\Carbon;
+use App\Models\TourHasGuideList;
+use App\Models\Tour;
+use App\Models\LocationInTour;
+use Illuminate\Support\Facades\Http;
+
 class CorpListController extends Controller
 {
-    function checkTable(){
+    function checkTable()
+    {
         if (Schema::hasTable((new CorpList)->getTable())) {
             echo "Corp exists!";
-        } 
-        else{
+        } else {
             echo "Table does not exist!";
         }
-      }
+    }
+    function addTour(Request $request)
+    {
+        $locationInTourAPI = $request->location;
+        $tourData = [
+            "from_owner" => 'corp',
+            "owner_id" => session('userID')->account_id_account,
+            "name" => $request->tour_name,
+            "Release_date" => Carbon::now()->toDateString(),
+            "End_of_sale_date" => Carbon::now()->addDays(7)->toDateString(),
+            "start_tour_date" => $request->start_date,
+            "end_tour_date" => $request->end_date,
+            "price" => $request->price,
+            "tour_capacity" => $request->quantity,
+            "contect" => $request->contact,
+            "hotel" => $request->hotel,
+            "hotel_price" => $request->hotelPrice,
+            "description" => $request->description,
+            "travel_by" => $request->travel_by,
+            "status" => 'ongoing',
+            "offer_id_offer" => NULL,
+            "type_tour" => 'public'
+        ];
+        $tour = new Tour($tourData);
+        $tour->save();
+        $tourId = $tour->id_tour;
+        $tourHasGuideData = [
+            "guide_list_account_id_account" => session('userID')->account_id_account,
+            "tour_id_tour" => $tourId
+        ];
+        TourHasGuideList::insert($tourHasGuideData);
+        foreach ($locationInTourAPI as $api) {
+            $locationInTourData = [
+                "loc_api" => "https://tatdataapi.io/api/v2/places/$api",
+                "tour_id_tour" => $tourId
+            ];
+            LocationInTour::insert($locationInTourData);
+        }
+        return redirect('/guideHomePage');
+    }
 }
--- SEThaiTravel/app/Http/Controllers/GuideListController.php
+++ SEThaiTravel/app/Http/Controllers/GuideListController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Http\Request;
 use App\Models\GuideList;
@@ -11,21 +12,25 @@ use App\Models\Payment;
 use App\Models\Offer;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
+
 class GuideListController extends Controller
 {
-    function getHomePage(){
+    function getHomePage()
+    {
         return view('guide.home');
     }
-    function checkTourSale(Request $request){
+    function checkTourSale(Request $request)
+    {
         $userID = session('userID')->account_id_account;
-        $tourData = Tour::where('owner_id',$userID)->where('status','ongoing')->get();
+        $tourData = Tour::where('owner_id', $userID)->where('status', 'ongoing')->get();
         dd($tourData);
     }
-    function checkWorkInTour(Request $request){
+    function checkWorkInTour(Request $request)
+    {
         $userID = session('userID')->account_id_account;
-        $workInTourData = TourHasGuideList::where('guide_list_account_id_account',$userID)->get();//ได้มาหลายข้อมูลเป็น array แน่ๆ
-        $toursID =  $workInTourData->pluck('tour_id_tour');//ทำการนำ tourID ทั้งหมดที่ guide เคยทำมารวบอยู่ในตัวแปรเดียวเพื่อนำไป query ต่อ
-        $tourData = Tour::whereIn('id_tour',$toursID)->where('status','finish');//เอาทัวร์ที่เสร็จแล้วเพราะเป็นอดีต
+        $workInTourData = TourHasGuideList::where('guide_list_account_id_account', $userID)->get(); //ได้มาหลายข้อมูลเป็น array แน่ๆ
+        $toursID =  $workInTourData->pluck('tour_id_tour'); //ทำการนำ tourID ทั้งหมดที่ guide เคยทำมารวบอยู่ในตัวแปรเดียวเพื่อนำไป query ต่อ
+        $tourData = Tour::whereIn('id_tour', $toursID)->where('status', 'finish'); //เอาทัวร์ที่เสร็จแล้วเพราะเป็นอดีต
         // return view('workHistory',compact('tourData'));
     }
     // function checkAllPayment(Request $request){
@@ -33,11 +38,13 @@ class GuideListController extends Controller
     //     $toursIOwn = Tour::where('owner_id',$userID)->where('status','ongoing')->get();
     //     $paymentInTour = Payment::where('booking_Tour_id_Tour',);
     // }  
-    function Offer(Request $request){
+    function Offer(Request $request)
+    {
         $requestTourID = 1; // ของจริงจะได้มาจากตอนกด offer  ex $request->requestTourId
-        return view('offerPage',compact('requestTourID'));
+        return view('offerPage', compact('requestTourID'));
     }
-    function addOffer(Request $request){
+    function addOffer(Request $request)
+    {
         $requestTourID = $request->requestTourID;
         $offerData = [
             "request_tour_id_request_tour" => $requestTourID,
@@ -99,45 +106,47 @@ class GuideListController extends Controller
             return response()->json(['error' => 'ไม่สามารถดึงข้อมูลสถานที่ท่องเที่ยวได้'], 500);
         }
     }
-    function getAddTour(){
+    function getAddTour()
+    {
         return view('guide.addTour');
     }
-    function addTour (Request $request){
-        $locationInTourAPI =$request->location;
-       $tourData = [
-        "from_owner" => 'guide',
-        "owner_id" => session('userID')->account_id_account,
-        "name" =>$request->tour_name,
-        "Release_date"=>Carbon::now()->toDateString(),
-        "End_of_sale_date"=>Carbon::now()->addDays(7)->toDateString(),
-        "start_tour_date"=>$request->start_date,
-        "end_tour_date"=>$request->end_date,
-        "price"=>$request->price,
-        "tour_capacity"=>$request->quantity,
-        "contect"=>$request->contact,
-        "hotel"=>$request->hotel,
-        "hotel_price"=>$request->hotelPrice,
-        "description"=>$request->description,
-        "travel_by"=>$request->travel_by,
-        "status"=>'ongoing',
-        "offer_id_offer"=>NULL,
-        "type_tour"=>'public'
-       ];
-       $tour= new Tour($tourData);
-       $tour->save();
-       $tourId = $tour->id_tour;
-       $tourHasGuideData = [
-        "guide_list_account_id_account"=>session('userID')->account_id_account,
-        "tour_id_tour"=> $tourId
-       ];
-       TourHasGuideList::insert($tourHasGuideData);
-       foreach($locationInTourAPI as $api){
-        $locationInTourData =[
-            "loc_api"=>"https://tatdataapi.io/api/v2/places/$api",
-            "tour_id_tour"=> $tourId
+    function addTour(Request $request)
+    {
+        $locationInTourAPI = $request->location;
+        $tourData = [
+            "from_owner" => 'guide',
+            "owner_id" => session('userID')->account_id_account,
+            "name" => $request->tour_name,
+            "Release_date" => Carbon::now()->toDateString(),
+            "End_of_sale_date" => Carbon::now()->addDays(7)->toDateString(),
+            "start_tour_date" => $request->start_date,
+            "end_tour_date" => $request->end_date,
+            "price" => $request->price,
+            "tour_capacity" => $request->quantity,
+            "contect" => $request->contact,
+            "hotel" => $request->hotel,
+            "hotel_price" => $request->hotelPrice,
+            "description" => $request->description,
+            "travel_by" => $request->travel_by,
+            "status" => 'ongoing',
+            "offer_id_offer" => NULL,
+            "type_tour" => 'public'
         ];
-        LocationInTour::insert($locationInTourData);
-       }
-       return redirect('/guideHomePage');
+        $tour = new Tour($tourData);
+        $tour->save();
+        $tourId = $tour->id_tour;
+        $tourHasGuideData = [
+            "guide_list_account_id_account" => session('userID')->account_id_account,
+            "tour_id_tour" => $tourId
+        ];
+        TourHasGuideList::insert($tourHasGuideData);
+        foreach ($locationInTourAPI as $api) {
+            $locationInTourData = [
+                "loc_api" => "https://tatdataapi.io/api/v2/places/$api",
+                "tour_id_tour" => $tourId
+            ];
+            LocationInTour::insert($locationInTourData);
+        }
+        return redirect('/guideHomePage');
     }
 }
