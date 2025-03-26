1738a5366f3a1f1af398abeadc5fdcf1639869c2
chumsaeng6520501034 <chumsaeng.j@ku.th>
2025-03-27 02:36:34 +0700

    edit crop detailoffer

--- SEThaiTravel/app/Http/Controllers/CorpListController.php
+++ SEThaiTravel/app/Http/Controllers/CorpListController.php
@@ -552,14 +552,28 @@ class CorpListController extends Controller
             ->where('id_who_offer', $idAccount)
             ->where('request_tour_id_request_tour', $request->requestID)
             ->get();
-        $offerInRequest= DB::table('offer')
-            ->where('request_tour_id_request_tour', $request->requestID)
-            ->get();
-        $RequestDetail = DB::table('request_tour')
+        $RequestDetail = RequestTour::join('user_list', 'request_tour.user_list_account_id_account', '=', 'user_list.account_id_account')
             ->where('id_request_tour', $request->requestID)
+            ->select('request_tour.*', 'user_list.name as uName', 'user_list.surname', 'user_list.phonenumber')  // เลือกคอลัมน์ทั้งหมดจากทั้ง 2 ตาราง
             ->first();
-            dd($offerByMe, $offerInRequest,$RequestDetail);
-        return view('corporation.detailMyOffer', compact('offerByMe'), compact('RequestDetail'), compact('offerInRequest'));
+        $offerInRequest = Offer::where('request_tour_id_request_tour', $request->requestID)->get();
+            $offerData = [];
+        foreach ($offerInRequest as $offer) {
+            switch ($offer->from_who_offer) {
+                case 'corp':
+                    $offerData[] = $offer->join('corp_list', 'corp_list.account_id_account', '=', 'offer.id_who_offer')
+                        ->where('corp_list.account_id_account', $offer->id_who_offer)
+                        ->first();
+                    break;
+                case 'guide':
+                    $offerData[] = $offer->join('guide_list', 'guide_list.account_id_account', '=', 'offer.id_who_offer')
+                        ->where('guide_list.account_id_account', $offer->id_who_offer)
+                        ->first();
+                    break;
+            }
+        }
+            // dd($offerByMe, $offerInRequest,$RequestDetail);
+        return view('corporation.detailMyOffer', compact('offerByMe','RequestDetail','offerData'));
     }
 
     //เสร็จแล้ว
