a11dec4b756e0b6bd2cba3157bf5c6db9ac92d01
Kamolwanaom <bbba96248@gmail.com>
2025-03-26 22:07:35 +0700

    edit

--- SEThaiTravel/resources/views/components/sidebarCustomer.blade.php
+++ SEThaiTravel/resources/views/components/sidebarCustomer.blade.php
@@ -23,7 +23,7 @@
 
     <!-- Sidebar -->
     <aside id="sidebar"
-        class="fixed top-0 left-0 w-64 h-screen bg-blue-900 text-white shadow-lg flex flex-col transform z-50 transition-transform duration-300 ">
+        class="fixed top-0 left-0 w-64 h-screen bg-gray-900 text-white shadow-lg flex flex-col transform z-50 transition-transform duration-300 ">
         <!-- User Profile -->
         <div class="p-6 text-center">
             <a href="/userProfile">
@@ -34,14 +34,14 @@
         </div>
 
         <!-- Menu Items -->
-        <nav class="flex flex-col space-y-2">
-            <a href="/home" class="block py-3 px-6 hover:bg-blue-700 transition duration-300">SEARCH</a>
-            <a href="/addTour" class="block py-3 px-6 hover:bg-blue-700 transition duration-300">ADD TOUR</a>
-            <a href="/myRequest" class="block py-3 px-6 hover:bg-blue-700 transition duration-300">MY REQUEST</a>
-            <a href="/history" class="block py-3 px-6 hover:bg-blue-700 transition duration-300">HISTORY</a>
-            <a href="/myBooking" class="block py-3 px-6 hover:bg-blue-700 transition duration-300">MY BOOKING</a>
-            <a href="/calendar" class="block py-3 px-6 hover:bg-blue-700 transition duration-300">CALENDAR</a>
-            <a href="/payments" class="block py-3 px-6 hover:bg-blue-700 transition duration-300">MY PAYMENT</a>
+        <nav class="flex flex-col space-y-1">
+            <a href="/home" class="block py-3 px-6 hover:bg-gray-700 transition duration-300 rounded">SEARCH</a>
+            <a href="/addTour" class="block py-3 px-6 hover:bg-gray-700 transition duration-300 rounded">ADD TOUR</a>
+            <a href="/myRequest" class="block py-3 px-6 hover:bg-gray-700 transition duration-300 rounded">MY REQUEST</a>
+            <a href="/history" class="block py-3 px-6 hover:bg-gray-700 transition duration-300 rounded">HISTORY</a>
+            <a href="/myBooking" class="block py-3 px-6 hover:bg-gray-700 transition duration-300 rounded">MY BOOKING</a>
+            <a href="/calendar" class="block py-3 px-6 hover:bg-gray-700 transition duration-300 rounded">CALENDAR</a>
+            <a href="/payments" class="block py-3 px-6 hover:bg-gray-700 transition duration-300 rounded">MY PAYMENT</a>
         </nav>
 
         <!-- Log Out -->
