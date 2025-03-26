ea71a4d4c601423b91a3c529fb5887168030f7f9
Kamolwanaom <bbba96248@gmail.com>
2025-03-27 00:51:16 +0700

    Update searchRequest.blade.php

--- SEThaiTravel/resources/views/guide/searchRequest.blade.php
+++ SEThaiTravel/resources/views/guide/searchRequest.blade.php
@@ -71,7 +71,7 @@
     <!-- Navbar -->
     <div id="mainContent">
         <nav id="navbar" class="fixed top-0 left-0 w-full p-4 z-[60] transition-all duration-300">
-            <div class="max-w-7xl mx-auto flex flex-col space-y-3 p-4 bg-[#205781] rounded-lg">
+            <div class="max-w-7xl mx-auto flex flex-col space-y-3 p-4  bg-white/10 backdrop-blur-2xl rounded-xl ">
                 <form action="/guideSearchFilter" method="GET">
                     <!-- บรรทัดแรก: แบรนด์ + ช่องค้นหา -->
                     <div class="flex justify-between items-center w-full">
